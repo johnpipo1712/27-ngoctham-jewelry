2f7c604395d69fa3237e8f3b3f8f288fe2f59fe5
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-05 17:10:53 +0000

    Edit and add View,Controller

--- Project/P_Code/GJS/GJS/W_GJS/Controllers/HomeController.cs
+++ Project/P_Code/GJS/GJS/W_GJS/Controllers/HomeController.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using W_GJS.Models;
 namespace W_GJS.Controllers
 {
     public class HomeController : Controller
     {
+        public GJSEntities Db_gsj;
         public ActionResult Index()
         {
+            Db_gsj = new GJSEntities();
             return View();
         }
 
         public ActionResult About()
         {
+            Db_gsj = new GJSEntities();
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -22,6 +25,7 @@ namespace W_GJS.Controllers
 
         public ActionResult Contact()
         {
+            Db_gsj = new GJSEntities();
             ViewBag.Message = "Your contact page.";
 
             return View();
