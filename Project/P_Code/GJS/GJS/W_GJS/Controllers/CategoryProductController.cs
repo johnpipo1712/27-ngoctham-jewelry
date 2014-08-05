f7ea4982d31f7b943abed315bce64a5d34c18f0f
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-05 17:12:19 +0000

    Add

---
+++ Project/P_Code/GJS/GJS/W_GJS/Controllers/CategoryProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using W_GJS.Models;
+namespace W_GJS.Controllers
+{
+    public class CategoryProductController : Controller
+    {
+        //
+        // GET: /CategoryProduct/
+        public GJSEntities Db_gsj;
+        public ActionResult Index()
+        {
+            Db_gsj = new GJSEntities();
+            return View(Db_gsj.O_CATEGORY_PRODUCT);
+        }
+	}
+}
---
+++ Project/P_Code/GJS/GJS/W_GJS/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using W_GJS.Models;
+namespace W_GJS.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+
+        // GET: /Employee/
+        public GJSEntities Db_gsj;
+        public ActionResult Index()
+        {
+            Db_gsj = new GJSEntities();
+            return View(Db_gsj.M_EMPLOYEE);
+        }
+	}
+}
