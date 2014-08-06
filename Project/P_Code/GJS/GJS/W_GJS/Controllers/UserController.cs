43c71d3eaf25b4d18e869851661d0c111559649c
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-06 14:25:08 +0000

    ADD CODE UPDATE

--- Project/P_Code/GJS/GJS/W_GJS/Controllers/UserController.cs
+++ Project/P_Code/GJS/GJS/W_GJS/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using W_GJS.Models;
@@ -16,5 +18,96 @@ namespace W_GJS.Controllers
             Db_gsj = new GJSEntities();
             return View(Db_gsj.O_SEO);
         }
-	}
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(S_USER USER)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Db_gsj = new GJSEntities();
+                USER.ACTIVE = true;
+                USER.STATUS = 0;
+                USER.CREATEDATE = DateTime.Now;
+                Db_gsj.Entry(USER).State = EntityState.Added;
+                Db_gsj.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(USER);
+            }
+        }
+        [HttpGet]
+        public ActionResult Edit(int? USER_CD)
+        {
+            if (USER_CD == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Db_gsj = new GJSEntities();
+            S_USER USER_edit = Db_gsj.S_USER.Single(t => t.USER_CD == USER_CD);
+            if (USER_edit == null)
+                return HttpNotFound();
+            return View(USER_edit);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(S_USER USER)
+        {
+            if (ModelState.IsValid)
+            {
+                Db_gsj = new GJSEntities();
+                S_USER USER_edit = Db_gsj.S_USER.Single(t => t.USER_CD == USER.USER_CD);
+                USER_edit.USER_PASS = USER.USER_PASS;
+                USER_edit.USER_NAME = USER.USER_NAME;
+                Db_gsj.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(USER);
+            }
+        }
+
+
+        public ActionResult Deactive([Bind(Include = "USER_CD")]S_USER USER)
+        {
+            if (USER == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Db_gsj = new GJSEntities();
+            USER = Db_gsj.S_USER.Single(t => t.USER_CD == USER.USER_CD);
+            USER.ACTIVE = false;
+            Db_gsj.Entry(USER).State = EntityState.Modified;
+            Db_gsj.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Active([Bind(Include = "USER_CD")]S_USER USER)
+        {
+            if (USER == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Db_gsj = new GJSEntities();
+            USER = Db_gsj.S_USER.Single(t => t.USER_CD == USER.USER_CD);
+            USER.ACTIVE = true;
+            Db_gsj.Entry(USER).State = EntityState.Modified;
+            Db_gsj.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
 }
