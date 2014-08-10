0af03425b766e75115f70c71c2ed84dd1418d521
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-10 09:24:37 +0000

    thêm các file cần thiết cho hệ thống

---
+++ trunk/Project/P_Code/GJS/GJS/W_GJS/Hubs/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(W_GJS.Hubs.Startup))]
+
+namespace W_GJS.Hubs
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
