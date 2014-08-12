d1ac6fe7e77b19a0e6ef9af1c9effdb60d099e44
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-12 16:35:33 +0000

    THÊM CÁC MODEL CẦN THIẾT ĐỂ QUẢN LÝ USER

---
+++ trunk/Project/P_Code/GJS/GJS/W_GJS/Models/LoginModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+
+namespace W_GJS.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Tên đăng nhập")]
+        [RegularExpression(@"\w\w*\b(?=(\s|\r|\n|$))", ErrorMessage = "Vui lòng không nhập kí tự đặt biệt")]
+        public string USER_NAME { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"\w\w*\b(?=(\s|\r|\n|$))", ErrorMessage = "Vui lòng không nhập kí tự đặt biệt")]
+        [Display(Name = "Mật khẩu")]
+        public string USER_PASS { get; set; }
+
+    }
+}
