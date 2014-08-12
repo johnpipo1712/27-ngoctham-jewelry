57392558d3b48947e9a1c94e88aa04a2a440813a
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-12 15:57:19 +0000

    chỉnh sửa phần HomeController

--- Project/P_Code/GJS/GJS/W_GJS/Controllers/HomeController.cs
+++ Project/P_Code/GJS/GJS/W_GJS/Controllers/HomeController.cs
@@ -15,19 +15,123 @@ namespace W_GJS.Controllers
             return View();
         }
 
+        public ActionResult Branches()
+        {
+            return View();
+        }
+
+        public ActionResult DiamondAssess()
+        {
+            return View();
+        }
+
+        public ActionResult ColoredStonesAssess()
+        {
+            return View();
+        }
+
+        public ActionResult LaserInscription()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult NewsDetail()
+        {
+            return View();
+        }
+
+        public ActionResult NewsGold()
+        {
+            return View();
+        }
+
+        public ActionResult NewsFinancial()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
-            Db_gsj = new GJSEntities();
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
+        public ActionResult AboutCompany()
+        {
+            ViewBag.Message = "Giới thiệu công ty.";
+
+            return View();
+        }
+
+        public ActionResult AboutAchievements()
+        {
+            ViewBag.Message = "Thành tựu.";
+
+            return View();
+        }
+
+        public ActionResult AboutDevelopment()
+        {
+            ViewBag.Messagetham = "Quá trình phát triển.";
+
+            return View();
+        }
+
+        public ActionResult AboutLicense()
+        {
+            ViewBag.Message = "Công ty TNHH Ngọc thẩm.";
+
+            return View();
+        }
+
+        public ActionResult AboutVision()
+        {
+            ViewBag.Message = "Tầm nhìn và phát triển.";
+
+            return View();
+        }
+
+
         public ActionResult Contact()
         {
-            Db_gsj = new GJSEntities();
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+
+        public ActionResult ShowAlbum()
+        {
+            return View();
+        }
+
+        public ActionResult Catalog()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
             return View();
         }
         public ActionResult Chat()
