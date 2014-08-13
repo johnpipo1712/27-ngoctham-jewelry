23e6d5633dc3d9bf2830a55849cc95f03e2389f1
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-13 15:10:18 +0000

    CHỉnh sửa đổ category theo database

--- Project/P_Code/GJS/GJS/W_GJS/Models/O_PRODUCT.cs
+++ Project/P_Code/GJS/GJS/W_GJS/Models/O_PRODUCT.cs
@@ -39,14 +39,16 @@ namespace W_GJS.Models
         public Nullable<bool> ACTIVE { get; set; }
         public Nullable<System.DateTime> CREATEDATE { get; set; }
         public Nullable<long> CATEGORY_PRODUCT_DETAIL_CD { get; set; }
+
+        public string SIZE { get; set; }
     
         public virtual ICollection<D_ORDER_DETAIL> D_ORDER_DETAIL { get; set; }
         public virtual ICollection<D_PRODUCT_DETAIL> D_PRODUCT_DETAIL { get; set; }
         public virtual O_CATEGORY_GRANULES O_CATEGORY_GRANULES { get; set; }
+        public virtual O_CATEGORY_PRODUCT O_CATEGORY_PRODUCT { get; set; }
         public virtual ICollection<O_PRODUCT_HIGHLIGHTS> O_PRODUCT_HIGHLIGHTS { get; set; }
         public virtual ICollection<O_PRODUCT_NEW> O_PRODUCT_NEW { get; set; }
         public virtual ICollection<O_PRODUCT_SELLING> O_PRODUCT_SELLING { get; set; }
         public virtual O_CATEGORY_PRODUCT_DETAIL O_CATEGORY_PRODUCT_DETAIL { get; set; }
-        public virtual O_CATEGORY_PRODUCT O_CATEGORY_PRODUCT { get; set; }
     }
 }
