da4f4d17327bc3099f80ca745eba27556cd7e3ce
vinh.thien0301@gmail.com <vinh.thien0301@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-15 03:59:59 +0000

    save register

--- Project/P_Code/GJS/GJS/W_GJS/Models/O_CUSTOMER.cs
+++ Project/P_Code/GJS/GJS/W_GJS/Models/O_CUSTOMER.cs
@@ -32,7 +32,7 @@ namespace W_GJS.Models
         public Nullable<long> STATUS { get; set; }
         public Nullable<bool> ACTIVE { get; set; }
         public Nullable<System.DateTime> CREATEDATE { get; set; }
-        public Nullable<byte> SUBSCRIBE { get; set; }
+        public Nullable<bool> SUBSCRIBE { get; set; }
     
         public virtual ICollection<O_ORDER> O_ORDER { get; set; }
         public virtual ICollection<O_USER_CUSTOMER> O_USER_CUSTOMER { get; set; }
