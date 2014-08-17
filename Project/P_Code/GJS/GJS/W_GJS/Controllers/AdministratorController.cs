1a28586ceca9041996b99f78795246c846c08e88
vinh.thien0301@gmail.com <vinh.thien0301@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-17 11:43:37 +0000

    add cde

---
+++ Project/P_Code/GJS/GJS/W_GJS/Controllers/AdministratorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using W_GJS.Models;
+namespace W_GJS.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        //
+        // GET: /BRANCH/
+        public GJSEntities Db_gsj;
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
