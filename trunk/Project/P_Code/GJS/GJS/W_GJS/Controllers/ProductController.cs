e5feb23a23cf38a996bab2e1fc7d002307ea803d
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-18 18:01:37 +0000

    CHỉnh sua xong phan san pham mai thu hien phan tin tuc va cac phan con lai

--- trunk/Project/P_Code/GJS/GJS/W_GJS/Controllers/ProductController.cs
+++ trunk/Project/P_Code/GJS/GJS/W_GJS/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using W_GJS.Models;
+using W_GJS.General;
 namespace W_GJS.Controllers
 {
     public class ProductController : Controller
@@ -21,10 +22,17 @@ namespace W_GJS.Controllers
         [HttpGet]
         public ActionResult Create()
         {
+            Db_gsj = new GJSEntities();
+            var query = Db_gsj.O_CATEGORY_PRODUCT_DETAIL.ToList();
+            ViewBag.Dropdownlist = new SelectList(query.AsEnumerable(), "CATEGORY_PRODUCT_DETAIL_CD", "CATEGORY_PRODUCT_DETAIL_NAME", 1);
+            var queryG = Db_gsj.O_CATEGORY_GRANULES.ToList();
+            ViewBag.DropdownlistG = new SelectList(queryG.AsEnumerable(), "CATEGORY_GRANULES_CD", "CATEGORY_GRANULES_NAME", 1);
+      
             return View();
         }
 
         [HttpPost]
+        [ValidateInput(false)]
         [ValidateAntiForgeryToken]
         public ActionResult Create(O_PRODUCT PRODUCT
             , String file1
@@ -36,71 +44,113 @@ namespace W_GJS.Controllers
 
             if (ModelState.IsValid)
             {
-                Db_gsj = new GJSEntities();
-                PRODUCT.ACTIVE = true;
-                PRODUCT.STATUS = 0;
-                PRODUCT.CREATEDATE = DateTime.Now;
-                Db_gsj.Entry(PRODUCT).State = EntityState.Added;
-                Db_gsj.SaveChanges();
-                if(file1 != "")
+                bool checkImage = false;
+                checkImage = Process.CheckExtensionImg(PRODUCT.URL_IMAGE);
+                if (file1 != "")
                 {
-                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
-                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
-                    dproudct.O_PRODUCT = PRODUCT;
-                    dproudct.STATUS = 0;
-                    dproudct.ACTIVE = true;
-                    dproudct.CREATEDATE = DateTime.Now;
-                    dproudct.URL_IMAGE = file1;
-                    Db_gsj.SaveChanges();
-
-
+                    checkImage = Process.CheckExtensionImg(file1);
                 }
                 if (file2 != "")
                 {
-                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
-                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
-                    dproudct.O_PRODUCT = PRODUCT;
-                    dproudct.STATUS = 0;
-                    dproudct.ACTIVE = true;
-                    dproudct.CREATEDATE = DateTime.Now;
-                    dproudct.URL_IMAGE = file2;
-                    Db_gsj.SaveChanges();
+                    checkImage = Process.CheckExtensionImg(file2);
                 }
                 if (file3 != "")
                 {
-                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
-                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
-                    dproudct.O_PRODUCT = PRODUCT;
-                    dproudct.STATUS = 0;
-                    dproudct.ACTIVE = true;
-                    dproudct.CREATEDATE = DateTime.Now;
-                    dproudct.URL_IMAGE = file3;
-                    Db_gsj.SaveChanges();
+                    checkImage = Process.CheckExtensionImg(file3);
                 }
                 if (file4 != "")
                 {
-                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
-                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
-                    dproudct.O_PRODUCT = PRODUCT;
-                    dproudct.STATUS = 0;
-                    dproudct.ACTIVE = true;
-                    dproudct.CREATEDATE = DateTime.Now;
-                    dproudct.URL_IMAGE = file4;
-                    Db_gsj.SaveChanges();
+                    checkImage = Process.CheckExtensionImg(file4);
+
                 }
                 if (file5 != "")
                 {
-                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
-                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
-                    dproudct.O_PRODUCT = PRODUCT;
-                    dproudct.STATUS = 0;
-                    dproudct.ACTIVE = true;
-                    dproudct.CREATEDATE = DateTime.Now;
-                    dproudct.URL_IMAGE = file5;
-                    Db_gsj.SaveChanges();
+                    checkImage = Process.CheckExtensionImg(file5);
                 }
-                return RedirectToAction("Index");
+                if (checkImage == false)
+                {
+                    Db_gsj = new GJSEntities();
+                    PRODUCT.ACTIVE = true;
+                    PRODUCT.STATUS = 0;
+                    PRODUCT.CREATEDATE = DateTime.Now;
+                    PRODUCT.O_CATEGORY_PRODUCT = Db_gsj.O_CATEGORY_PRODUCT_DETAIL.Single(t => t.CATEGORY_PRODUCT_DETAIL_CD == PRODUCT.CATEGORY_PRODUCT_DETAIL_CD).O_CATEGORY_PRODUCT;
+                    PRODUCT.CATEGORY_PRODUCT_CD = PRODUCT.CATEGORY_PRODUCT_CD;
+                    Db_gsj.Entry(PRODUCT).State = EntityState.Added;
+                    Db_gsj.SaveChanges();
+                    if (file1 != "")
+                    {
+                        D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                        dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                        dproudct.O_PRODUCT = PRODUCT;
+                        dproudct.STATUS = 0;
+                        dproudct.ACTIVE = true;
+                        dproudct.CREATEDATE = DateTime.Now;
+                        dproudct.URL_IMAGE = file1;
+                        Db_gsj.Entry(dproudct).State = EntityState.Added;
+                        Db_gsj.SaveChanges();
 
+
+                    }
+                    if (file2 != "")
+                    {
+                        D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                        dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                        dproudct.O_PRODUCT = PRODUCT;
+                        dproudct.STATUS = 0;
+                        dproudct.ACTIVE = true;
+                        dproudct.CREATEDATE = DateTime.Now;
+                        dproudct.URL_IMAGE = file2;
+                        Db_gsj.Entry(dproudct).State = EntityState.Added;
+                       
+                        Db_gsj.SaveChanges();
+                    }
+                    if (file3 != "")
+                    {
+                        D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                        dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                        dproudct.O_PRODUCT = PRODUCT;
+                        dproudct.STATUS = 0;
+                        dproudct.ACTIVE = true;
+                        dproudct.CREATEDATE = DateTime.Now;
+                        dproudct.URL_IMAGE = file3;
+                        Db_gsj.Entry(dproudct).State = EntityState.Added;
+                       
+                        Db_gsj.SaveChanges();
+                    }
+                    if (file4 != "")
+                    {
+                        D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                        dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                        dproudct.O_PRODUCT = PRODUCT;
+                        dproudct.STATUS = 0;
+                        dproudct.ACTIVE = true;
+                        dproudct.CREATEDATE = DateTime.Now;
+                        dproudct.URL_IMAGE = file4;
+                        Db_gsj.Entry(dproudct).State = EntityState.Added;
+                       
+                        Db_gsj.SaveChanges();
+                    }
+                    if (file5 != "")
+                    {
+                        D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                        dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                        dproudct.O_PRODUCT = PRODUCT;
+                        dproudct.STATUS = 0;
+                        dproudct.ACTIVE = true;
+                        dproudct.CREATEDATE = DateTime.Now;
+                        dproudct.URL_IMAGE = file5;
+                        Db_gsj.Entry(dproudct).State = EntityState.Added;
+                       
+                        Db_gsj.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Vui lòng kiểm tra các đường dẫn hình ảnh");
+                      
+                    return View(PRODUCT);
+                }
             }
             else
             {
@@ -116,6 +166,11 @@ namespace W_GJS.Controllers
             }
             Db_gsj = new GJSEntities();
             O_PRODUCT PRODUCT_edit = Db_gsj.O_PRODUCT.Single(t => t.PRODUCT_CD == PRODUCT_CD);
+            var query = Db_gsj.O_CATEGORY_PRODUCT_DETAIL.ToList();
+            ViewBag.Dropdownlist = new SelectList(query.AsEnumerable(), "CATEGORY_PRODUCT_DETAIL_CD", "CATEGORY_PRODUCT_DETAIL_NAME", PRODUCT_edit.CATEGORY_PRODUCT_DETAIL_CD);
+            var queryG = Db_gsj.O_CATEGORY_GRANULES.ToList();
+            ViewBag.DropdownlistG = new SelectList(query.AsEnumerable(), "CATEGORY_GRANULES_CD", "CATEGORY_GRANULES_NAME", PRODUCT_edit.CATEGORY_GRANULES_CD);
+      
             if (PRODUCT_edit == null)
                 return HttpNotFound();
             return View(PRODUCT_edit);
@@ -123,11 +178,42 @@ namespace W_GJS.Controllers
         }
 
         [HttpPost]
+        [ValidateInput(false)]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(O_PRODUCT PRODUCT)
+        public ActionResult Edit(O_PRODUCT PRODUCT
+            , String file1
+            , String file2
+            , String file3
+            , String file4
+            , String file5)
         {
             if (ModelState.IsValid)
             {
+                 bool checkImage = false;
+                checkImage = Process.CheckExtensionImg(PRODUCT.URL_IMAGE);
+                if (file1 != "")
+                {
+                    checkImage = Process.CheckExtensionImg(file1);
+                }
+                if (file1 != "")
+                {
+                    checkImage = Process.CheckExtensionImg(file2);
+                }
+                if (file1 != "")
+                {
+                    checkImage = Process.CheckExtensionImg(file3);
+                }
+                if (file1 != "")
+                {
+                    checkImage = Process.CheckExtensionImg(file4);
+
+                }
+                if (file1 != "")
+                {
+                    checkImage = Process.CheckExtensionImg(file5);
+                }
+                if (checkImage == false)
+                {
                 Db_gsj = new GJSEntities();
                 O_PRODUCT PRODUCT_edit = Db_gsj.O_PRODUCT.Single(t => t.PRODUCT_CD == PRODUCT.PRODUCT_CD);
                 PRODUCT_edit.PRODUCT_CODE = PRODUCT.PRODUCT_CODE;
@@ -135,8 +221,82 @@ namespace W_GJS.Controllers
                 PRODUCT_edit.PRICE = PRODUCT.PRICE;
                 PRODUCT_edit.QUANTITY = PRODUCT.QUANTITY;
                 Db_gsj.SaveChanges();
-                return RedirectToAction("Index");
+                Db_gsj.D_PRODUCT_DETAIL.RemoveRange(PRODUCT_edit.D_PRODUCT_DETAIL);
+                if (file1 != "")
+                {
+                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                    dproudct.O_PRODUCT = PRODUCT;
+                    dproudct.STATUS = 0;
+                    dproudct.ACTIVE = true;
+                    dproudct.CREATEDATE = DateTime.Now;
+                    dproudct.URL_IMAGE = file1;
+                    Db_gsj.Entry(dproudct).State = EntityState.Added;
+                         
+                    Db_gsj.SaveChanges();
 
+
+                }
+                if (file2 != "")
+                {
+                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                    dproudct.O_PRODUCT = PRODUCT;
+                    dproudct.STATUS = 0;
+                    dproudct.ACTIVE = true;
+                    dproudct.CREATEDATE = DateTime.Now;
+                    dproudct.URL_IMAGE = file2;
+                    Db_gsj.Entry(dproudct).State = EntityState.Added;
+                          
+                    Db_gsj.SaveChanges();
+                }
+                if (file3 != "")
+                {
+                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                    dproudct.O_PRODUCT = PRODUCT;
+                    dproudct.STATUS = 0;
+                    dproudct.ACTIVE = true;
+                    dproudct.CREATEDATE = DateTime.Now;
+                    dproudct.URL_IMAGE = file3;
+                    Db_gsj.Entry(dproudct).State = EntityState.Added;
+                         
+                    Db_gsj.SaveChanges();
+                }
+                if (file4 != "")
+                {
+                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                    dproudct.O_PRODUCT = PRODUCT;
+                    dproudct.STATUS = 0;
+                    dproudct.ACTIVE = true;
+                    dproudct.CREATEDATE = DateTime.Now;
+                    dproudct.URL_IMAGE = file4;
+                    Db_gsj.Entry(dproudct).State = EntityState.Added;
+                         
+                    Db_gsj.SaveChanges();
+                }
+                if (file5 != "")
+                {
+                    D_PRODUCT_DETAIL dproudct = new D_PRODUCT_DETAIL();
+                    dproudct.PRODUCT_CD = PRODUCT.PRODUCT_CD;
+                    dproudct.O_PRODUCT = PRODUCT;
+                    dproudct.STATUS = 0;
+                    dproudct.ACTIVE = true;
+                    dproudct.CREATEDATE = DateTime.Now;
+                    dproudct.URL_IMAGE = file5;
+                    Db_gsj.Entry(dproudct).State = EntityState.Added;
+                         
+                    Db_gsj.SaveChanges();
+                }
+                return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Vui lòng kiểm tra các đường dẫn hình ảnh");
+
+                    return View(PRODUCT);
+                }
             }
             else
             {
@@ -172,6 +332,85 @@ namespace W_GJS.Controllers
             Db_gsj.Entry(PRODUCT).State = EntityState.Modified;
             Db_gsj.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public JsonResult CheckNewAjax(long? PRODUCT_CD,String CHECK)
+        {
+            Db_gsj = new GJSEntities();
+            if(CHECK == "true")
+            {
+                O_PRODUCT Product = Db_gsj.O_PRODUCT.Single(t => t.PRODUCT_CD == PRODUCT_CD);
+                O_PRODUCT_NEW PRODUCTNEW = new O_PRODUCT_NEW();
+                PRODUCTNEW.O_PRODUCT = Product;
+                PRODUCTNEW.PRODUCT_CD = Product.PRODUCT_CD;
+                Db_gsj.Entry(PRODUCTNEW).State = EntityState.Added;
+                Db_gsj.SaveChanges();
+            }
+            else
+            {
+                List<O_PRODUCT_NEW> PRODUCTNEW = Db_gsj.O_PRODUCT_NEW.Where(t=>t.PRODUCT_CD == PRODUCT_CD).ToList();
+                Db_gsj.O_PRODUCT_NEW.RemoveRange(PRODUCTNEW);
+                Db_gsj.SaveChanges();
+            }
+            return Json(1);
+        }
+        public JsonResult CheckHightlightAjax(long? PRODUCT_CD, String CHECK)
+        {
+            Db_gsj = new GJSEntities();
+            if (CHECK == "true")
+            {
+                O_PRODUCT Product = Db_gsj.O_PRODUCT.Single(t => t.PRODUCT_CD == PRODUCT_CD);
+                O_PRODUCT_HIGHLIGHTS PRODUCTHIGHLIGHTS = new O_PRODUCT_HIGHLIGHTS();
+                PRODUCTHIGHLIGHTS.O_PRODUCT = Product;
+                PRODUCTHIGHLIGHTS.PRODUCT_CD = Product.PRODUCT_CD;
+                Db_gsj.Entry(PRODUCTHIGHLIGHTS).State = EntityState.Added;
+                Db_gsj.SaveChanges();
+            }
+            else
+            {
+                List<O_PRODUCT_HIGHLIGHTS> PRODUCTHIGHLIGHTS = Db_gsj.O_PRODUCT_HIGHLIGHTS.Where(t => t.PRODUCT_CD == PRODUCT_CD).ToList();
+                Db_gsj.O_PRODUCT_HIGHLIGHTS.RemoveRange(PRODUCTHIGHLIGHTS);
+                Db_gsj.SaveChanges();
+            }
+            return Json(1);
+        }
+        public JsonResult CheckSellingAjax(long? PRODUCT_CD, String CHECK)
+        {
+            Db_gsj = new GJSEntities();
+            if (CHECK == "true")
+            {
+                O_PRODUCT Product = Db_gsj.O_PRODUCT.Single(t => t.PRODUCT_CD == PRODUCT_CD);
+                O_PRODUCT_SELLING PRODUCTSELLING = new O_PRODUCT_SELLING();
+                PRODUCTSELLING.O_PRODUCT = Product;
+                PRODUCTSELLING.PRODUCT_CD = Product.PRODUCT_CD;
+                Db_gsj.Entry(PRODUCTSELLING).State = EntityState.Added;
+                Db_gsj.SaveChanges();
+            }
+            else
+            {
+                List<O_PRODUCT_SELLING> PRODUCTSELLING = Db_gsj.O_PRODUCT_SELLING.Where(t => t.PRODUCT_CD == PRODUCT_CD).ToList();
+                Db_gsj.O_PRODUCT_SELLING.RemoveRange(PRODUCTSELLING);
+                Db_gsj.SaveChanges();
+            }
+            return Json(1);
+        }
+        public JsonResult CheckDisplayAjax(long? PRODUCT_CD, String CHECK)
+        {
+            Db_gsj = new GJSEntities();
+            if (CHECK == "true")
+            {
+                O_PRODUCT Product = Db_gsj.O_PRODUCT.Single(t => t.PRODUCT_CD == PRODUCT_CD);
+                Product.STATUS = 1;
+                Db_gsj.Entry(Product).State = EntityState.Modified;
+                Db_gsj.SaveChanges();
+            }
+            else
+            {
+                O_PRODUCT Product = Db_gsj.O_PRODUCT.Single(t => t.PRODUCT_CD == PRODUCT_CD);
+                Product.STATUS = 0;
+                Db_gsj.Entry(Product).State = EntityState.Modified;
+                Db_gsj.SaveChanges();
+            }
+            return Json(1);
         }
     }
 }
---
+++ trunk/Project/P_Code/GJS/GJS/W_GJS/General/Process.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace W_GJS.General
+{
+    public class Process
+    {
+       
+        public static bool CheckExtensionImg(string extension)
+        {
+            string[] extensions = { "jpg", "jpeg", "gif", "png", "bmp", "jpe", "jfif", "tif", "JPG", "JPEG", "GIF", "PNG", "BMP", "JPE", "JFIF", "TIF" };
+
+            int count = 0;
+            foreach (string item in extensions)
+            {
+                if (extension == item)
+                    count++;
+            }
+            if (count == 1)
+                return true;
+
+            return false;
+        }
+    }
+}
