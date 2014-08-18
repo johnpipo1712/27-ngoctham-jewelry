03f6097d3ece1867d0f9bdd23d9b52d90fafb604
vinh.thien0301@gmail.com <vinh.thien0301@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-18 03:29:43 +0000

    add register

--- Project/P_Code/GJS/GJS/W_GJS/Scripts/self-scripts.js
+++ Project/P_Code/GJS/GJS/W_GJS/Scripts/self-scripts.js
@@ -1,15 +1,47 @@
-﻿inProgressLogin = false;
-
+﻿// LOGIN
+inProgressLogin = false;
 $( "#login-button" ).click(function() {
     if (!inProgressLogin) {
         inProgressLogin = true;
         $("#loadingDivLogin").show();
+        $("#error-login").hide();
         $.post($(this).data("action"), $('#form-login').serialize(),
-            function (data) { 
-                $("#error-login").append(data.ErrorString);
-                $("#loadingDivLogin").hide();
-                inProgressLogin = false;
+            function (data) {
+                if (data.RoleOrFailed == 0) {
+                    $("#error-login").html(data.ErrorString);
+                    $("#loadingDivLogin").hide();
+                    inProgressLogin = false;
+                    $("#error-login").show();
+
+                } else {
+                    $("#loadingDivLogin").hide();
+                    window.location.href = $("#login-button").data("success-action");
+                }
         });
     }
 });
 
+// REGISTER
+inProgressRegister = false;
+$( "#register-button" ).click(function() {
+    if (!inProgressRegister) {
+        inProgressRegister = true;
+        $("#loadingDivRegister").show();
+        $("#error-register").hide();
+        $.post($(this).data("action"), $('#form-register').serialize(),
+            function (data) {
+                if (data.HasError) {
+                    $("#error-register").html(data.ErrorString);
+                    $("#loadingDivRegister").hide();
+                    inProgressRegister = false;
+                    $("#error-register").show();
+
+                } else {
+                    $("#loadingDivRegister").hide();
+                    $("#RegisterModal").modal('hide');
+                    $("#registerSuccesfully").modal('show');
+                    window.location.href = $("#register-button").data("success-action");
+                }
+        });
+    }
+});
