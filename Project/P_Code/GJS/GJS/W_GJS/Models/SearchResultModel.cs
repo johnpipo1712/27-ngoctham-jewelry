b56b9513edf7664a515013db1ee0afbcde4c9b00
vinh.thien0301@gmail.com <vinh.thien0301@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-20 23:31:26 +0000

    fix search

---
+++ Project/P_Code/GJS/GJS/W_GJS/Models/SearchResultModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace W_GJS.Models
+{
+    public class SearchResultModel
+    {
+        public string HtmlString { get; set; }
+        
+    }
+}
