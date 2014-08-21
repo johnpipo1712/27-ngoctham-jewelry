9d3aa13b6b27bc3e03cc379ee85b92eb3af54b13
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-21 10:34:06 +0000

    CHỉnh sửa lại phần phân quyền

---
+++ Project/P_Code/GJS/GJS/W_GJS/Self/customer/js/script.js
@@ -0,0 +1,25 @@
+﻿
+
+
+function CheckPst(CUSTOMER_CD, control) {
+
+    $.ajax({
+        url: '/Customer/CheckPstAjax',
+
+        type: 'POST',
+
+        data: { CUSTOMER_CD: CUSTOMER_CD, CHECK: control.checked },
+
+        dataType: 'json',
+
+        success: function (results) {
+
+        },
+        error: function (results) {
+            alert('Lỗi');
+        }
+
+
+    });
+
+}
