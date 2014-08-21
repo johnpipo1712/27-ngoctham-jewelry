2aafde41512f8814e6ecd790c10c84154a9caeab
vinh.thien0301@gmail.com <vinh.thien0301@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-21 00:00:49 +0000

    fix search

--- trunk/Project/P_Code/GJS/GJS/W_GJS/Models/SearchResultModel.cs
+++ trunk/Project/P_Code/GJS/GJS/W_GJS/Models/SearchResultModel.cs
@@ -8,6 +8,7 @@ namespace W_GJS.Models
     public class SearchResultModel
     {
         public string HtmlString { get; set; }
+        public int NumberOfItemFound {  get; set; }
         
     }
 }
