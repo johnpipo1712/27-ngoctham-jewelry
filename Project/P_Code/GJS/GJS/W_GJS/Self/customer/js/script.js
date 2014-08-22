76f59fdcefe4b0c816b5a78857a1872081cf9649
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-22 15:26:37 +0000

    Chinh phan User

--- Project/P_Code/GJS/GJS/W_GJS/Self/customer/js/script.js
+++ Project/P_Code/GJS/GJS/W_GJS/Self/customer/js/script.js
@@ -1,14 +1,12 @@
 ﻿
-
-
-function CheckPst(CUSTOMER_CD, control) {
-
-    $.ajax({
-        url: '/Customer/CheckPstAjax',
+function changePST(CUSTOMER_CD, control) {
+   
+     $.ajax({
+        url: '/Customer/ChangePstAjax',
 
         type: 'POST',
 
-        data: { CUSTOMER_CD: CUSTOMER_CD, CHECK: control.checked },
+        data: { CUSTOMER_CD: CUSTOMER_CD, change: control.options[control.selectedIndex].value },
 
         dataType: 'json',
 
@@ -23,3 +21,27 @@ function CheckPst(CUSTOMER_CD, control) {
     });
 
 }
+
+
+//function CheckPst(CUSTOMER_CD, control) {
+
+//    $.ajax({
+//        url: '/Customer/CheckPstAjax',
+
+//        type: 'POST',
+
+//        data: { CUSTOMER_CD: CUSTOMER_CD, CHECK: control.checked },
+
+//        dataType: 'json',
+
+//        success: function (results) {
+
+//        },
+//        error: function (results) {
+//            alert('Lỗi');
+//        }
+
+
+//    });
+
+//}
