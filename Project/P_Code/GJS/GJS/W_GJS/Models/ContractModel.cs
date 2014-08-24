bd41062960844eb887a6f7e9a6c67fba25e069a1
nhtai1712@gmail.com <nhtai1712@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-24 07:33:50 +0000

    chỉnh sua them gửi mail

---
+++ Project/P_Code/GJS/GJS/W_GJS/Models/ContractModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace W_GJS.Models
+{
+    public class ContractModel
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Note { get; set; }
+
+
+    }
+}
