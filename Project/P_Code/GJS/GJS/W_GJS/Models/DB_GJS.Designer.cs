b3884fc5be0eb4b7586d2955e0b9defcf46d2df1
vinh.thien0301@gmail.com <vinh.thien0301@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-08-29 02:58:55 +0000

    revert code

--- Project/P_Code/GJS/GJS/W_GJS/Models/DB_GJS.Designer.cs
+++ Project/P_Code/GJS/GJS/W_GJS/Models/DB_GJS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Thien\Desktop\webvang\Project\P_Code\GJS\GJS\W_GJS\Models\DB_GJS.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\gjs\GJS\Project\P_Code\GJS\GJS\W_GJS\Models\DB_GJS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
