b02431be039dfc64346209c1d4de293012c59cd3
vinh.thien0301@gmail.com <vinh.thien0301@gmail.com@aaedee05-a6c1-7742-7f9c-036c0b13aeff>
2014-10-24 16:06:32 +0000

    fix code

--- trunk/Project/P_Code/GJS/GJS/W_GJS/Controllers/CategoryProductController.cs
+++ trunk/Project/P_Code/GJS/GJS/W_GJS/Controllers/CategoryProductController.cs
@@ -16,7 +16,7 @@ namespace W_GJS.Controllers
         public ActionResult Index()
         {
             Db_gsj = new GJSEntities();
-            return View(Db_gsj.O_CATEGORY_PRODUCT.OrderByDescending(x => x.CREATEDATE));
+            return View(Db_gsj.O_CATEGORY_PRODUCT.Where(t => t.ACTIVE == true).OrderByDescending(x => x.CREATEDATE));
         }
         [HttpGet]
         public ActionResult Create()
